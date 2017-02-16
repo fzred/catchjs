e0fdaf1b80f981903b658cf5919d0076ec848ab1
fzred <1179750402@qq.com>
2017-02-16 19:09:36 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const webpack = require('webpack')
 const packager = require('./package.json')
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-    entry: './st.js',
+    entry: './catch.js',
     output: {
         path: './dist',
-        filename: `st.${packager.version}.js`,
+        filename: isProd ? `catch.${packager.version}.js` : 'catch.js',
     },
     plugins: [],
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (isProd) {
     module.exports.plugins = module.exports.plugins.concat([
         new webpack.DefinePlugin({
             'process.env': {
