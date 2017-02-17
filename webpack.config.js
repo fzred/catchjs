9efb820c92d1d55509fd93473c855725650463fd
fzred <1179750402@qq.com>
2017-02-17 08:12:36 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const packager = require('./package.json')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    entry: './catch.js',
+    entry: './src/catch.js',
     output: {
         path: './dist',
         filename: isProd ? `catch.${packager.version}.js` : 'catch.js',
