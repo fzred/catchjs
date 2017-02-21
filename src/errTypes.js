a20a8d8ee25719c41b348599703a8179e8adef17
fzred <1179750402@qq.com>
2017-02-22 07:04:43 +0800

    errtype

---
+++ src/errTypes.js
@@ -0,0 +1,16 @@
+/**
+ * 错误类型
+ */
+
+
+export const ERROR_RUNTIME = 1
+export const ERROR_LOAD_SCRIPT = 2
+export const ERROR_LOAD_STYLE = 3
+export const ERROR_LOAD_IMG = 4
+export const ERROR_LOAD_AUDIO = 5
+export const ERROR_LOAD_VIDEO = 6
+
+export const ERROR_XHR = 7
+export const ERROR_FETCH = 8
+
+
